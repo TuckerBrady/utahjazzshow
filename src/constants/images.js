e3ff08da024f95426658688e8711cc49c15a0cce
TuckerBrady <tuck.al.brady@gmail.com>
2022-12-31 14:19:32 -0700

    sets up header

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import facebookRound from '../assets/facebook-round.png';
+import twitter from '../assets/twitter.png';
+import instagramFilled from '../assets/instagram-filled.png';
+import profile from '../assets/profile.png';
+
+export default {
+    facebookRound,
+    twitter,
+    instagramFilled,
+    profile
+}
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from "./Navbar.scss";
 
+import { images } from "../constants";
+
 export default function Navbar() {
   return (
-    <div className="top">
-      <div className="topLeft">Left</div>
-      <div className="topCenter">Center</div>
-      <div className="topRight">Right</div>
+    <div className="navbar">
+      <div className="navbarLeft">
+        <i class="navbarIcon__logo fa-brands fa-square-facebook" />
+        <i class="navbarIcon__logo fa-brands fa-square-instagram" />
+        <i class="navbarIcon__logo fa-brands fa-square-twitter" />
+      </div>
+      <div className="navbarCenter">
+        <ul className="navbarList">
+          <li className="navbarList__item">Home</li>
+          <li className="navbarList__item">About</li>
+          <li className="navbarList__item">Contact</li>
+          <li className="navbarList__item">Write</li>
+          <li className="navbarList__item">Logout</li>
+        </ul>
+      </div>
+      <div className="navbarRight">
+        <img className="navbarImage" src={images.profile} alt="profile" />
+        <i class="NavbarIcon__search fa-sharp fa-solid fa-magnifying-glass" />
+      </div>
     </div>
   );
 }
