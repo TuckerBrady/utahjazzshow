705323f2018bd2147a08d05156eb780d2d15c508
TuckerBrady <tuck.al.brady@gmail.com>
2022-12-31 12:35:02 -0700

    inital creation and commit

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "./Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <div className="top">
+      <div className="topLeft">Left</div>
+      <div className="topCenter">Center</div>
+      <div className="topRight">Right</div>
+    </div>
+  );
+}
