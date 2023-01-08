a86502520dd92c4622ef84c4f078a1358e90b375
TuckerBrady <tuck.al.brady@gmail.com>
2023-01-07 19:24:27 -0700

    adds api folder and connects to mongodb

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useCreateIndex: true
+});
+
+app.listen("3000", ()=>{
+    console.log("Backend Is Running")
+})
