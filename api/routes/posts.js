b8680b4af3e10fd65c7356b07a1b31fb30026cdc
TuckerBrady <tuck.al.brady@gmail.com>
2023-01-16 23:40:31 -0700

    finishes setting up rest api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,27 +14,69 @@ router.post("/", async(req, res) => {
 });
 
 //Update Post
+router.put("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(
+                    req.params.id, 
+                    { $set: req.body }, 
+                    { new: true }
+                );
+                res.status(200).json(updatedPost);
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } else {
+            res.status(401).json("You are not allowed to update this post.");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //Delete Post
 router.delete("/:id", async(req, res) => {
-    if (req.body.userId === req.params.id) {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
         try {
-            await User.findByIdAndDelete(req.params.id)
-            res.status(200).json("User has been deleted");
+            await post.delete();
+            res.status(200).json("Post has been deleted");
         } catch (err) {
             res.status(500).json(err);
         }
     } else {
-        res.status(401).json("You do not have permission to delete this user");
+        res.status(401).json("You do not have permission to delete this post");
     }
 });
 
 //Get Post
 router.get("/:id", async(req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...otherStuff } = user._doc;
-        res.status(200).json(otherStuff);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Get all Posts
+router.get("/", async(req, res) => {
+    const username = req.query.user;
+    const category = req.query.category;
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({ username: username });
+        } else if (category) {
+            posts = await Post.find({ categories: {
+                $in: [category]
+            }});
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
